8cd0e38ae2f80b9e7560357b62631d9ecc8c13bf
Steven Orola <stevenorola@gmail.com>
2018-04-19 14:46:37 -0700

    fixed boostrap so 12 column grid is used

--- src/index.js
+++ src/index.js
@@ -16,23 +16,29 @@ class App extends Component {
 			selectedVideo: null
 		};
 		
-		YTSearch( {key: API_KEY, term: 'surfboards'}, (videos) => {
+		this.videoSearch('surfboards');
+		
+	}
+	
+	videoSearch(term) {
+		YTSearch( {key: API_KEY, term: term}, (videos) => {
 			this.setState({ 
 				videos: videos,
 				selectedVideo: videos[0]
 			});
-		});
-		
+		});	
 	}
 	
 	render() {
 		return (
 			<div>
-				<SearchBar />
-				<VideoDetail video={this.state.selectedVideo}/>
-				<VideoList 
-					onVideoSelect = { selectedVideo => this.setState({selectedVideo}) },
-					videos = {this.state.videos} />
+				<SearchBar onSearchTermChange = {term => this.videoSearch(term)}/>
+				<div className="row">
+					<VideoDetail video={this.state.selectedVideo}/>
+					<VideoList 
+						onVideoSelect = { selectedVideo => this.setState({selectedVideo}) }
+						videos = {this.state.videos} />
+				</div>
 			</div>
 		);
 	}
